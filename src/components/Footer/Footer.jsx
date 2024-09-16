7cbf701e07b8b8aec7aea56c8645c6ed351307de
Suraj Chaudhari <theprogrammer0086@gmail.com>
2024-09-16 18:52:46 +0530

    modified footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-// import { Data } from './footerData';
+import { Data } from './footerData';
 import '../../Styles/footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <section className="footer-section-1">
-        {Data.sections.map((section, index) => (
-          <div key={index}>
+        {Data.sections.map((section) => (
+          <div key={section.id}>
             <h3>{section.title}</h3>
             <ul>
-              {section.links.map(({ link, index }) => (
+              {section.links.map((link) => (
                 link ? (
-                  <li key={index}>
+                  <li key={link.id}>
                     <a href={link.url}>{link.text}</a>
                   </li>
                 ) : null // Or render a placeholder if link is undefined
@@ -26,8 +26,8 @@ const Footer = () => {
           <p>&copy; 2024 surajchaudhari03™</p>
         </div>
         <div className="social-icons">
-          {Data.socialLinks.map((link, index) => (
-            <a key={index} href={link?.url}>
+          {Data.socialLinks.map((link) => (
+            <a key={link.id} href={link?.url}>
               <i className={link.icon}></i>
             </a>
           ))}
@@ -38,49 +38,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-
-const Data = {
-  sections: [
-    {
-      title: 'Company',
-      links: [
-        {  url: "#", text: "About"},
-        {  url: "#", text: "Careers"},
-        {  url: "#", text: "Brand Center",},
-        {  url: "#", text: "Blog"},
-      ],
-    },
-    {
-      title: "Help Center",
-      links: [
-        {  url: "#", text: "Discord Server",},
-        {  url: "#", text: "Twitter"},
-        {  url: "#", text: "Facebook"},
-        {  url: "#", text: "Contact Us"},
-      ],
-    },
-    {
-      title: 'Legal',
-      links: [
-        {  url: "#", text: "Privacy Policy"},
-        {  url: "#", text: "Licensing"},
-        {  url: "#", text: "Terms & Conditions"}
-      ],
-    },
-    {
-      title: "Download",
-      links: [
-        {  url: "#", text: "iOS"},
-        {  url: "#", text: "Android"},
-        {  url: "#", text: "Windows"},
-        {  url: "#", text: "MacOS"},
-      ],
-    }
-  ],
-  socialLinks: [
-    { url: "#", icon: "fab fa-github" },
-    { url: "#", icon: "fab fa-linkedin" },
-    { url: "#", icon: "fab fa-instagram" }
-  ],
-};
--- src/components/Footer/footerData.js
+++ src/components/Footer/footerData.js
@@ -1,29 +1,33 @@
 export const Data = {
-    sections: [
-      {
-        title: 'Company',
-        links: [
-          { text: "About", url: "#" },
-          { text: "Careers", url: "#" },
-          { text: "Brand Center", url: "#" },
-          { text: "Blog", url: "#" },
-        ],
-      },
-      {
-        title: "Help Center",
-        links: [
-          { text: "Discord Server", url: "#" },
-          { text: "Twitter", url: "#" },
-          { text: "Facebook", url: "#" },
-          { text: "Contact Us", url: "#" },
-        ],
-      },
-      // ... more sections
-    ],
-    socialLinks: [
-      { url: "#", icon: "fab fa-facebook" },
-      { url: "#", icon: "fab fa-instagram" },
-      { url: "#", icon: "fab fa-twitter" },
-      // ... more social icons
-    ],
-  };
+  sections: [
+    {
+      id: 1,
+      title: 'Company',
+      links: [
+        { id: 1, url: "#", text: "About"},
+        { id: 2, url: "#", text: "Blog"},
+      ],
+    },
+    {
+      id: 2,
+      title: "Help Center",
+      links: [
+        { id: 1, url: "#", text: "Contact Us"},
+      ],
+    },
+    {
+      id: 3,
+      title: 'Legal',
+      links: [
+        { id: 1, url: "#", text: "Privacy Policy"},
+        { id: 2,  url: "#", text: "Licensing"},
+        { id: 3,  url: "#", text: "Terms & Conditions"}
+      ],
+    }
+  ],
+  socialLinks: [
+    { id: 1, url: "#", icon: "fab fa-github" },
+    { id: 2, url: "#", icon: "fab fa-linkedin" },
+    { id: 3, url: "#", icon: "fab fa-instagram" }
+  ],
+};
